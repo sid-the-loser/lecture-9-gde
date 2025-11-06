71c98dfc76ed2ec9de09f3797778f20a2d3d0503
Sidharth <sidharthsuresh07@gmail.com>
2025-11-06 18:38:10 -0500

    Add export presets for Windows and Linux, and refine object pooling logic

--- scripts/object_pooling.gd
+++ scripts/object_pooling.gd
@@ -2,9 +2,10 @@ extends Node
 
 @export var pool_size: int = 10
 @export var spawn_locations: Array[Node2D]
-@export var spawn_time: float = 2
+@export var spawn_time: float = 0.5
 
 var spawn_timer = 0
+var spawned_objects = 0
 
 const ENEMY_CAR = preload("uid://c7oa8pkcy2gs")
 
@@ -13,16 +14,17 @@ var df_spawn = false
 func _process(delta: float) -> void:
 	spawn_timer += delta
 	
-	if spawn_timer >= spawn_time and !df_spawn:
+	if (spawn_timer >= spawn_time and !df_spawn and spawned_objects < pool_size):
 		spawn_enemy()
 		spawn_timer = 0
-		df_spawn = true
 		
 		
 func spawn_enemy():
-	var location: Node2D = spawn_locations[randi_range(0, len(spawn_locations))]
+	var location: Node2D = spawn_locations[randi_range(0, len(spawn_locations)-1)]
 
 	var enemy = ENEMY_CAR.instantiate()
+	
+	add_child(enemy)
 	
 	enemy.position = location.position
 
